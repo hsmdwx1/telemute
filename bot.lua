00a7e613a8d3dfb8873e2b444a9e91cdceb48a95
hsmdwx1 <hsmdewx1@gmail.com>
2017-02-21 03:53:03 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -8,8 +8,8 @@ redis = (loadfile "./libs/redis.lua")()
 serpent = require('serpent')
 serp = require 'serpent'.block
 sudo_users = {
-  238773538,
-  173606679,
+  312600227,
+  317576202,
   0
 }
 
